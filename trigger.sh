9048ef0a3bc8c58d06dda29625d6e6c1ea700d0c
Clément MATHIEU <clement@unportant.info>
2023-01-31 22:42:10 +0100

    Avoid custom git alias

--- trigger.sh
+++ trigger.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -xe
 
-git co .
+git checkout .
 mvn clean compile
 sed -i 's/10/11/'  src/main/java/Main.java 
 rm target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
